1db878c18938cb4d7ec4e24dd7f384a018b43813
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2016-12-27 15:42:05 +0800

    website

--- AfterDarkServer/AddReview.php
+++ AfterDarkServer/AddReview.php
@@ -15,6 +15,14 @@ $food = filter_input(INPUT_POST, 'food');
 $service = filter_input(INPUT_POST, 'service');
 $ambience = filter_input(INPUT_POST, 'ambience');
 
+//$User_ID = "2";
+//$Bar_ID = "0";
+//$avg = "3";
+//$price = "2";
+//$food = "4";
+//$service = "3";
+//$ambience = "3";
+
 if($Bar_ID == null)
 {
     Output::Fail("no bar id");
@@ -44,7 +52,38 @@ $to_set_types = "ssssssssss";
 
 $success = Database::StatementInsertWhere("reviews",$set_columns, $set_values, $to_set_types);
 
-if($success)
+//also, update bar's average review
+//step 1: Get bar's reviews
+$barReviews = Database::SelectWhereColumn("Bar_Rating_Avg,Bar_Rating_Food,Bar_Rating_Price,Bar_Rating_Ambience,Bar_Rating_Service", "bar_info", "Bar_ID", $Bar_ID);
+//step 2: Get number of bar reviews given
+
+$totalReviewCount = Database::Count("reviews", "Bar_ID", $Bar_ID);
+//step 3: take current average * number + new review / total number
+$barAvg = $barReviews[0]['Bar_Rating_Avg'];
+$barPrice = $barReviews[0]['Bar_Rating_Price'];
+$barFood = $barReviews[0]['Bar_Rating_Food'];
+$barService = $barReviews[0]['Bar_Rating_Service'];
+$barAmbience = $barReviews[0]['Bar_Rating_Ambience'];
+
+
+$newBarAvg = ($barAvg*$totalReviewCount + $avg)/($totalReviewCount+1);
+$newBarPrice = ($barPrice*$totalReviewCount + $price)/($totalReviewCount+1);
+$newBarFood = ($barFood*$totalReviewCount + $food)/($totalReviewCount+1);
+$newBarService = ($barService*$totalReviewCount + $service)/($totalReviewCount+1);
+$newBarAmbience = ($barAmbience*$totalReviewCount + $ambience)/($totalReviewCount+1);
+
+//step 4: update bar's new average
+$update_columns = array("Bar_Rating_Avg","Bar_Rating_Food","Bar_Rating_Price","Bar_Rating_Ambience","Bar_Rating_Service");
+$update_values = array($newBarAvg,$newBarFood,$newBarPrice,$newBarAmbience,$newBarService);
+$update_types = "sssss";
+$updateSuccess = Database::StatementUpdateWhere("bar_info", $update_columns, $update_values, $update_types,["Bar_ID"], [$Bar_ID], "s");
+
+//if($updateSuccess)
+//{
+//    echo "avg:".$newBarAvg." food:".$newBarFood." price:".$newBarPrice." ambience:".$newBarAmbience." service:".$newBarService;
+//}
+
+if($success && $updateSuccess)
 {
     Output::Success("Review Added!");
 }

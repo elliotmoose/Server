4fbb9fd91e51ae030aa6df999e3d14231226bb1d
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2016-12-31 09:38:31 +0800

    server update

--- AfterDarkServer/GetBarNonImageInfo.php
+++ AfterDarkServer/GetBarNonImageInfo.php
@@ -6,7 +6,7 @@ $con = Database::BeginConnection();
 
 $BarName = filter_input(INPUT_GET, "Bar_Name");
 
-$sql = sprintf("SELECT Bar_Description,Bar_Location_Latitude,Bar_Location_Longitude,OH_Monday,OH_Tuesday,OH_Wednesday,OH_Thursday,OH_Friday,OH_Saturday,OH_Sunday,Bar_Contact,Booking_Available FROM bar_info WHERE Bar_Name = \"%s\"",$BarName);
+$sql = sprintf("SELECT Bar_Description,Bar_Location_Latitude,Bar_Location_Longitude,OH_Monday,OH_Tuesday,OH_Wednesday,OH_Thursday,OH_Friday,OH_Saturday,OH_Sunday,Bar_Contact,Booking_Available,Bar_Website,Bar_Address FROM bar_info WHERE Bar_Name = \"%s\"",$BarName);
 
 $output = Database::QueryStringToArrayAssoc($sql);
 

48f042b5988a77fdafa1f2e4a4fbc57213da355b
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2017-01-16 23:34:55 +0800

    update

--- AfterDarkServer/HardLoadAllBars.php
+++ AfterDarkServer/HardLoadAllBars.php
@@ -7,8 +7,7 @@ require_once(__DIR__ . '/Database.php');
 
 Database::BeginConnection();
 
-$request = "Bar_Name,Bar_ID,Bar_Rating_Price,Bar_Rating_Ambience,Bar_Rating_Price,Bar_Rating_Food,Bar_Rating_Service,Bar_Rating_Avg,Bar_Description,Bar_Location_Latitude,Bar_Location_Longitude,OH_Monday,OH_Tuesday,OH_Wednesday,OH_Thursday,OH_Friday,OH_Saturday,OH_Sunday,Bar_Contact,Booking_Available,Bar_Website,Bar_Address,Bar_Tags,Bar_PriceDeterminant,lastUpdate,Bar_Rating_Count"
-        . "";
+$request = "Bar_Name,Bar_ID,Bar_Rating_Price,Bar_Rating_Ambience,Bar_Rating_Price,Bar_Rating_Food,Bar_Rating_Service,Bar_Rating_Avg,Bar_Description,Bar_Location_Latitude,Bar_Location_Longitude,OH_Monday,OH_Tuesday,OH_Wednesday,OH_Thursday,OH_Friday,OH_Saturday,OH_Sunday,Bar_Contact,Booking_Available,Bar_Website,Bar_Address,Bar_Tags,Bar_PriceDeterminant,lastUpdate,Bar_Rating_Count";
 
 $output = Database::Select($request, "bar_info");
 

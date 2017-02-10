32ed714f9307870e00bf63fcce0f401a204f95b8
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2017-02-10 13:04:48 +0800

    scripts update

--- AfterDarkServer/GetBarGalleryImage.php
+++ AfterDarkServer/GetBarGalleryImage.php
@@ -6,6 +6,17 @@ $con = Database::BeginConnection();
 $bar_ID = filter_input(INPUT_GET, "Bar_ID");
 $bar_image_index = filter_input(INPUT_GET, "Image_Index");
 
+if($bar_ID == null)
+{
+    Output::Fail("No Bar ID Given");    
+}
+
+if($bar_image_index == null)
+{
+    Output::Fail("No image index given");
+}
+
+
 $imageSource = "../AfterDarkServer/Bar_Images/$bar_ID/$bar_image_index.jpg";
 
 //redirect to image source

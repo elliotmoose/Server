c67258611007c654688d60014881bc6b52bfa401
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2017-05-07 20:53:17 +0800

    removed testing script

--- ShareDraw/Functions/GetCommentsForSubmission.php
+++ ShareDraw/Functions/GetCommentsForSubmission.php
@@ -7,10 +7,6 @@ $user_ID = filter_input(INPUT_POST, "user_ID");
 $group_ID = filter_input(INPUT_POST, "group_ID");
 $assignment_ID = filter_input(INPUT_POST, "assignment_ID");
 
-$user_ID = "1";
-$group_ID = "S13F";
-$assignment_ID = "6dmz4n";
-
 $dir = "../Groups/$group_ID/$assignment_ID/$user_ID/comments.txt";
 
 $output = Files::ReadJSONFile($dir);

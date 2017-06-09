5d84bc190f0637aa8c7ed3040ccd9616585808b0
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2017-06-09 16:10:38 +0800

    testing

--- Services/Update.php
+++ Services/Update.php
@@ -2,6 +2,7 @@
 //$op = exec("C:\MAMP\htdocs\GitPull.bat 2>&1");
 //$op = exec('C:\MAMP\htdocs\update.sh');
 
-$op = shell_exec('/var/www/html/update.sh');
+$op = shell_exec(‘./update.sh');
+
 echo nl2br($op);
 //$op = implode("\n", $op);

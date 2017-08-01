fef9a0b4f7508d0571ac87862034afe2595839ee
root <root@ip-172-31-36-247.us-west-2.compute.internal>
2017-08-01 23:38:40 +0000

    fixed hardloadallbars.php

--- AfterDarkServer/HardLoadAllBars.php
+++ AfterDarkServer/HardLoadAllBars.php
@@ -1,4 +1,4 @@
-yn<?php
+<?php
 
 
 //this soft load means bar data excluding: 
@@ -7,7 +7,7 @@ require_once(__DIR__ . '/Database.php');
 
 Database::BeginConnection();
 
-$request = "Bar_Name,Bar_ID,Bar_Rating_Price,Bar_Rating_Ambience,Bar_Rating_Price,Bar_Rating_Food,Bar_Rating_Service,Bar_Rating_Avg,Bar_Description,Bar_Location_Latitude,Bar_Location_Longitude,OH_Monday,OH_Tuesday,OH_Wednesday,OH_Thursday,OH_Friday,OH_Saturday,OH_Sunday,Bar_Contact,Booking_Available,Bar_Website,Bar_Address,Bar_Tags,Bar_PriceDeterminant,lastUpdate,Bar_Rating_Count,Exclusive";
+$request = "Bar_Name,Bar_ID,Bar_Rating_Price,Bar_Rating_Ambience,Bar_Rating_Price,Bar_Rating_Food,Bar_Rating_Service,Bar_Rating_Avg,Bar_Description,Bar_Location_Latitude,Bar_Location_Longitude,OH_Monday,OH_Tuesday,OH_Wednesday,OH_Thursday,OH_Friday,OH_Saturday,OH_Sunday,Bar_Contact,Booking_Available,Bar_Website,Bar_Address,Bar_Tags,Bar_PriceDeterminant,lastUpdate,Bar_Rating_Count,Exclusive,Enabled";
 
 $output = Database::Select($request, "bar_info");
 

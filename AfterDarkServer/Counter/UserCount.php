7fa4024d52641447fdf731a79ac1bc9103f2a54c
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2017-08-25 18:14:31 +0800

    improved counter

--- AfterDarkServer/Counter/UserCount.php
+++ AfterDarkServer/Counter/UserCount.php
@@ -7,15 +7,39 @@ Database::BeginConnection();
 
 $output = Database::Select("*","user_info");
 
+$singaporeUsers = array();
+$maxSingaporeUsers = array();
+foreach($output as $user)
+{    
+    $phoneNumber = $user["User_Contact"];        
+    $firstNumber = substr($phoneNumber,0,1);    
+    if($firstNumber == "8" || $firstNumber == "9")
+    {
+        array_push($singaporeUsers,$user);
+    }
+    else
+    {
+        if($phoneNumber == "NIL" || $phoneNumber == "")
+        {
+            array_push($maxSingaporeUsers,$user);
+        }
+    }
+    
+}
+
 $count = count($output);
+$sgCount = count($singaporeUsers);
+$maxSgCount = count($maxSingaporeUsers) + $sgCount;
 $date = date('d-m-Y H:i:s');
 
 $stamp = array();
 $stamp["date"] = $date;
 $stamp["count"] = $count;
+$stamp["SGCount"] = $sgCount . "-" . $maxSgCount;
 
 $file = fopen('count.txt','r');
 $stamps = json_decode(stream_get_contents($file));
+
 
 array_push($stamps,$stamp);
 
@@ -24,7 +48,8 @@ foreach($stamps as $eachStamp)
     $eachStamp = (array)$eachStamp;
     $thisDate = $eachStamp["date"];
     $thisCount = $eachStamp["count"];
-    echo "Date: " . $thisDate . " Count: " . $thisCount . '<br>';
+    $thisSGCount = $eachStamp["SGCount"];
+    echo "Date: " . $thisDate . " Users: " . $thisCount . " Est. SG Users: " . $thisSGCount . '<br>';
 }
 
 file_put_contents('count.txt',json_encode($stamps));
